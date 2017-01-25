17e38c9101ddd7934bebc62fc0fe38a42c58bdb8
Zachary Donato <zdonato@stevens.edu>
2017-01-25 13:59:47 -0500

    Adds mock data for use with pjson tutorial.

--- docs/build/app.bundle.js
+++ docs/build/app.bundle.js
@@ -62115,7 +62115,68 @@
 /* 179 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	var timeout = 100; // simulate server response time
+	var colors = [{
+	    value: 0,
+	    text: 'Red'
+	}, {
+	    value: 1,
+	    text: 'Orange'
+	}, {
+	    value: 2,
+	    text: 'Yellow'
+	}, {
+	    value: 3,
+	    text: 'Green'
+	}, {
+	    value: 4,
+	    text: 'Blue'
+	}, {
+	    value: 5,
+	    text: 'Purple'
+	}];
+
+	var things = [{
+	    color: 0,
+	    text: 'Apples'
+	}, {
+	    color: 0,
+	    text: 'Fire Trucks'
+	}, {
+	    color: 1,
+	    text: 'Oranges'
+	}, {
+	    color: 4,
+	    text: 'Sky'
+	}, {
+	    color: 4,
+	    text: 'Oceans'
+	}, {
+	    color: 4,
+	    text: 'BlueBerry'
+	}];
+	function mockAjax(request, callback) {
+	    setTimeout(function () {
+	        switch (request.uri) {
+	            case 'colors':
+	                callback(null, { Status: 'Success', data: colors });
+	                break;
+	            case 'things':
+	                callback(null, { Status: 'Success', data: things });
+	                break;
+	            default:
+	                callback({ Status: 'Error', message: 'Error' });
+	        }
+	    }, timeout);
+	}
+	exports.default = {
+	    ajax: mockAjax
+	};
 
 /***/ },
 /* 180 */
---
+++ public/src/app/dataservice.js
@@ -0,0 +1,71 @@
+const timeout = 100; // simulate server response time
+const colors = [
+    {
+        value: 0,
+        text: 'Red'
+    },
+    {
+        value: 1,
+        text: 'Orange'
+    },
+    {
+        value: 2,
+        text: 'Yellow'
+    },
+    {
+        value: 3,
+        text: 'Green'
+    },
+    {
+        value: 4,
+        text: 'Blue'
+    },
+    {
+        value: 5,
+        text: 'Purple'
+    }
+];
+
+const things = [
+    {
+        color: 0,
+        text: 'Apples'
+    },
+    {
+        color: 0,
+        text: 'Fire Trucks'
+    },
+    {
+        color: 1,
+        text: 'Oranges'
+    },
+    {
+        color: 4,
+        text: 'Sky'
+    },
+    {
+        color: 4,
+        text: 'Oceans'
+    },
+    {
+        color: 4,
+        text: 'BlueBerry'
+    }
+];
+function mockAjax(request, callback) {
+    setTimeout(() => {
+        switch (request.uri) {
+            case 'colors':
+                callback(null, { Status: 'Success', data: colors });
+                break;
+            case 'things':
+                callback(null, { Status: 'Success', data: things });
+                break;
+            default:
+                callback({ Status: 'Error', message: 'Error' });
+        }
+    }, timeout);
+}
+export default {
+    ajax: mockAjax
+};
