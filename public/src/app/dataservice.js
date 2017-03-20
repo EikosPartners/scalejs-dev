66e27c1a07b2c5c131b64d42b95467ecf22a1beb
Zachary Donato <zdonato@stevens.edu>
2017-03-20 15:22:40 -0400

    Changes stubbed dataservice to make ajax requests.

--- public/src/app/dataservice.js
+++ public/src/app/dataservice.js
@@ -55,6 +55,70 @@ const things = [
 
 var formData = null;
 
+// request can either be a string (url), or an object with request.url, and/or request.options
+// request.options can specify the options.type (GET/POST/ect), and options.data to send in request.
+function _ajax(request, callback) {
+    // Extract options and url from request:
+    let req = request || baseUri; // Default for request when falsy
+
+    const isRequestString = (typeof req === 'string' || req instanceof String),
+        // use request.options if exists, otherwise use {}
+        options = (!isRequestString && req.options) || {},
+        type = (options.type || 'GET').toUpperCase(),
+        data = req.data || req.data;
+
+    let url = (isRequestString ? req : req.url || '/') + (req.uri || '');
+
+    // Check if we're in the testing suite.
+    if (window.location.port === '9004' || window.location.protocol === 'file:') {
+        url = 'http://localhost:3000' + url;
+    }
+
+    // Create request:
+    req = new XMLHttpRequest();
+
+    req.withCredentials = true;
+
+    req.onreadystatechange = function () {
+        // Check to make sure request is completed, otherwise ignore:
+        if (this.readyState === XMLHttpRequest.DONE) {
+            let response = this.response;
+            if (this.status === 200) {
+                // Request was successful, now parse:
+                if (this.getResponseHeader('Content-Type').includes('application/json')) {
+                    try {
+                        response = JSON.parse(response);
+                    } catch (error) {
+                        // Failed to parse, error:
+                        return callback({
+                            error: error,
+                            status: this.status,
+                            message: response
+                        });
+                    }
+                }
+
+                callback(undefined, response);
+            } else {
+                // Request errored in some way, return error:
+                callback({
+                    error: this.statusText,
+                    status: this.status,
+                    message: response
+                });
+            }
+        }
+    };
+
+    // Open and send request:
+    req.open(type, url, true);
+
+    // Set options:
+    if (type !== 'GET') { req.setRequestHeader('Content-Type', 'application/json'); }
+
+    req.send(type !== 'GET' && JSON.stringify(data));
+}
+
 function mockAjax(request, callback) {
     setTimeout(() => {
         switch (request.uri) {
@@ -73,7 +137,9 @@ function mockAjax(request, callback) {
                 }
                 break;
             default:
-                callback({ Status: 'Error', message: 'Error' });
+                _ajax(request, (error, data) => {
+                    callback(error, data);
+                });
         }
     }, timeout);
 }
